3042740ca80d20ae8a605df75c07d47cd4bfefb9
itisMilan <abhaymilan8@gmail.com>
2023-06-10 16:42:47 +0530

    Added Cors

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const cors= require('cors');
-app.use(cors);
 const jsonServer = require('json-server');
 const server = jsonServer.create();
+const cors= require('cors');
+server.use(cors);
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
